bd567df9ff65565c17456ba3402144f6097868c0
toshitk <takahashi.t66@gmail.com>
2024-04-18 09:57:15 +0900

    refactor: aggregate table difinitions

--- app/dao/CompaniesDao.scala
+++ app/dao/CompaniesDao.scala
@@ -9,21 +9,11 @@ import play.api.db.slick.HasDatabaseConfigProvider
 import scala.concurrent.ExecutionContext
 import slick.jdbc.JdbcProfile
 
-class CompaniesDao @Inject()(protected val dbConfigProvider: DatabaseConfigProvider) extends HasDatabaseConfigProvider[JdbcProfile] {
-  import driver.api._
-
-  private val Companies = TableQuery[CompaniesTable]
+class CompaniesDao @Inject()(protected val dbConfigProvider: DatabaseConfigProvider)
+extends HasDatabaseConfigProvider[JdbcProfile] with Tables {
+  import profile.api._
 
   def all()(implicit ec: ExecutionContext): Future[Seq[Company]] = db.run(Companies.result)
 
   def insert(c: Company)(implicit ec: ExecutionContext): Future[Unit] = db.run(Companies += c).map { _ => () }
-
-  private class CompaniesTable(tag: Tag) extends Table[Company](tag, "COMPANIES") {
-
-    def id = column[Int]("ID", O.PrimaryKey, O.AutoInc)
-    def name = column[String]("NAME")
-    def address = column[String]("ADDRESS")
-
-    def * = (id.?, name, address) <> ((Company.apply _).tupled, Company.unapply)
-  }
 }
--- app/dao/UsersDao.scala
+++ app/dao/UsersDao.scala
@@ -9,21 +9,11 @@ import play.api.db.slick.HasDatabaseConfigProvider
 import scala.concurrent.ExecutionContext
 import slick.jdbc.JdbcProfile
 
-class UsersDao @Inject()(protected val dbConfigProvider: DatabaseConfigProvider) extends HasDatabaseConfigProvider[JdbcProfile] {
-  import driver.api._
-
-  private val Users = TableQuery[UsersTable]
+class UsersDao @Inject()(protected val dbConfigProvider: DatabaseConfigProvider)
+extends HasDatabaseConfigProvider[JdbcProfile] with Tables {
+  import profile.api._
 
   def all()(implicit ec: ExecutionContext): Future[Seq[User]] = db.run(Users.result)
 
-  def insert(user: User)(implicit ec: ExecutionContext): Future[Unit] = db.run(Users += user).map { _ => () }
-
-  private class UsersTable(tag: Tag) extends Table[User](tag, "USERS") {
-
-    def id = column[Int]("ID", O.PrimaryKey, O.AutoInc)
-    def name = column[String]("NAME")
-    def age = column[Int]("AGE")
-
-    def * = (id.?, name, age) <> ((User.apply _).tupled, User.unapply)
-  }
+  def insert(u: User)(implicit ec: ExecutionContext): Future[Unit] = db.run(Users += u).map { _ => () }
 }
