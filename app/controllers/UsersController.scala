741689414699c1c8aad00d406303cee467694003
toshitk <takahashi.t66@gmail.com>
2024-04-18 11:15:37 +0900

    feat: static response

--- app/controllers/UsersController.scala
+++ app/controllers/UsersController.scala
@@ -1,35 +1,34 @@
 package controllers
 
 import javax.inject._
-import play.api._
+// import play.api._
 import play.api.mvc._
-import play.api.db._
-import dao.UsersDao
-import models.User
-import play.api.data.Form
-import play.api.data.Forms._
-import scala.concurrent.ExecutionContext
+// import play.api.db._
+// import dao.UsersDao
+// import models.User
+// import play.api.data.Form
+// import play.api.data.Forms._
+// import scala.concurrent.ExecutionContext
+import scala.concurrent.Future
 
 @Singleton
-class UsersController @Inject()(usersDao: UsersDao, cc: ControllerComponents)(implicit ec: ExecutionContext) extends AbstractController(cc) {
+class UsersController @Inject()(cc: ControllerComponents) extends AbstractController(cc) {
 
   def index = Action.async { implicit request =>
-    usersDao.allWithCompany().map {
-        result => Ok(views.html.users(result))
-    }
+    Future.successful(Ok("OK!"))
   }
 
-  def create = Action.async { implicit request =>
-    val user: User = userForm.bindFromRequest.get
-    usersDao.insert(user).map(_ => Redirect(routes.UsersController.index))
-  }
+  // def create = Action.async { implicit request =>
+  //   val user: User = userForm.bindFromRequest.get
+  //   usersDao.insert(user).map(_ => Redirect(routes.UsersController.index))
+  // }
 
-  val userForm = Form(
-    mapping(
-      "name" -> nonEmptyText,
-      "age" -> number,
-      "companyId" -> optional(number)
-    )((name, age, companyId) => User(None, name, age, companyId))
-    (u => Some((u.name, u.age, u.companyId)))
-  )
+  // val userForm = Form(
+  //   mapping(
+  //     "name" -> nonEmptyText,
+  //     "age" -> number,
+  //     "companyId" -> optional(number)
+  //   )((name, age, companyId) => User(None, name, age, companyId))
+  //   (u => Some((u.name, u.age, u.companyId)))
+  // )
 }
