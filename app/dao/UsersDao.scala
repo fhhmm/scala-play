4fb8a24d982153969d6751996ed058c3267d8e3b
toshitk <takahashi.t66@gmail.com>
2024-04-18 10:08:27 +0900

    feat: add allWithCompany()

--- app/dao/UsersDao.scala
+++ app/dao/UsersDao.scala
@@ -15,5 +15,12 @@ extends HasDatabaseConfigProvider[JdbcProfile] with Tables {
 
   def all()(implicit ec: ExecutionContext): Future[Seq[User]] = db.run(Users.result)
 
+  def allWithCompany()(implicit ec: ExecutionContext): Future[Seq[(User, Option[Company])]] = {
+    val query = for {
+      (user, company) <- Users joinLeft Companies on (_.companyId === _.id)
+    } yield (user, company)
+    db.run(query.result)
+  }
+
   def insert(u: User)(implicit ec: ExecutionContext): Future[Unit] = db.run(Users += u).map { _ => () }
 }
